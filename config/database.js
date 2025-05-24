a14521c0d2b1788cc3df4cb66d74cd6817f97fc8
Charlotte-82 <charlotteregis1@gmail.com>
2025-05-24 14:20:40 +0200

    correction connexion db 3

--- config/database.js
+++ config/database.js
@@ -1,15 +1,15 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-console.log("DB_HOST:", process.env.DB_HOST);
-console.log("DB_PORT:", process.env.DB_PORT);
-console.log("DB_USER:", process.env.DB_USER);
-console.log("DB_NAME:", process.env.DB_NAME);
+console.log("DB_HOST:", process.env.MYSQLHOST);
+console.log("DB_PORT:", process.env.MYSQLPORT);
+console.log("DB_USER:", process.env.MYSQLUSER);
+console.log("DB_NAME:", process.env.MYSQLDATABASE);
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.MYSQLDATABASE,
+  process.env.MYSQLUSER,
+  process.env.MYSQLPASSWORD,
   {
     host: process.env.DB_HOST,
     dialect: "mysql",
