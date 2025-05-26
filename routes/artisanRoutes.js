c4474f15313318a755136a9528f3ada51106703b
Charlotte-82 <charlotteregis1@gmail.com>
2025-05-26 04:30:41 +0200

    correction connexion front 6

--- routes/artisanRoutes.js
+++ routes/artisanRoutes.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
+console.log("Attempting to load artisanController...");
+
 const artisanControllers = require("../controllers/artisanControllers");
+console.log("artisanController loaded:", typeof artisanController);
+console.log("getTopArtisans type:", typeof artisanController.getTopArtisans);
 
 router.get("/top", artisanControllers.getTopArtisans);
-console.log("GET /api/top received"); // Reçue !
+console.log("GET /api/top received");
 
 router.get("/filtres", artisanControllers.getArtisansFiltres);
 router.get("/", artisanControllers.getArtisans);
